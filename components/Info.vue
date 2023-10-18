4fd1c0ae0ef8689e9881c53db2ddd9e2f41fec72
probablyjassin <aouanijassin@gmail.com>
2023-10-18 12:26:15 +0200

    use nuxt content

--- components/Info.vue
+++ components/Info.vue
@@ -1,25 +1,13 @@
 <template>
 	<div class="absolute top-0 right-0 flex justify-center text-center mx-auto">
 		<div class="p-2 border rounded-lg bg-slate-200">
-            <button class="mt-2 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none" @click="toggleExpand">
+			<button class="mt-2 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none" @click="toggleExpand">
 				{{ expanded ? "Close" : "read me" }}
 			</button>
-            <div v-if="expanded" class="overflow-hidden w-auto transition-all duration-300">
-                <p>Well well welt</p>
-                <p>I caught the sound of a man named Zy0nix trying to roast the design of the website I made</p>
-                <p>So I decided to make it look better</p>
-                <p>I even made an animation for when you win the game. So thanks for the encouragement om</p>
-                <p>~ probably jassin</p>
-                <p class="mt-3">Here is the original version, (if you liked it more you can still use it) from which I will migrate the missing features over time: 
-					<a href="https://gaugedle.jassin.aouani.de/old" class="text-blue-500 underline">click here</a>
-				</p>
-				<div class="mt-3">
-					<p class="text-xl">Release notes:</p>
-					<ul>
-						<li>Gotta add Neuvillete soon</li>
-						<li>Gotta add all the functionality</li>
-					</ul>
-				</div>
+			<div v-if="expanded" class="overflow-hidden w-auto transition-all duration-300">
+				<main>
+					<ContentDoc />
+				</main>
 			</div>
 		</div>
 	</div>
@@ -41,5 +29,4 @@
 </script>
 
 <style scoped>
-	/* Add any custom styles here */
 </style>
---
+++ content/index.md
@@ -0,0 +1,4 @@
+Here is the original version, (if you liked it more you can still use it) from which I will migrate the missing features over time:
+<a href="https://gaugedle.jassin.aouani.de/old" class="text-blue-500 underline">click here</a>
+<li>Gotta add Neuvillete soon</li>
+<li>Gotta add all the functionality</li>
