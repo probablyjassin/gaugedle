a8e23afcfff47faf85d86c64768d5c662416ea81
probablyjassin <aouanijassin@gmail.com>
2023-10-21 17:56:40 +0200

    adjust logging

--- composables/useLogs.js
+++ composables/useLogs.js
@@ -2,10 +2,10 @@ let messages = ref();
 
 export function useLogs() {
 	function getLogs() {
-		fetch(`https://api.github.com/repos/probablyjassin/gaugedle/commits?per_page=3`)
+		fetch(`https://api.github.com/repos/probablyjassin/gaugedle/commits?per_page=10`)
 			.then((response) => response.json())
 			.then((commits) => {
-				messages.value = commits.map((commit) => commit.commit.message);
+				messages.value = commits.map((commit) => commit.commit.message).filter((message) => message.startsWith("-")).slice(0, 3);
 			})
 			.catch((error) => {
 				console.error("Error fetching commit messages:", error);
