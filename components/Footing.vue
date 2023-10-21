3f54877041bf55f22b2c10432331d7ee232342ba
probablyjassin <aouanijassin@gmail.com>
2023-10-21 17:18:59 +0200

    recreated footer

--- components/Footing.vue
+++ components/Footing.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
-
-    </div>
+    <footer class="flex justify-center">
+        <div class=" absolute bottom-0 sm:text-base text-[11px] text-slate-300">
+            <p>
+                version 2.3 | made and maintained by 
+                <a href="https://github.com/probablyjassin" class="underline text-blue-300">
+                    probablyjassin
+                </a>, idea by 
+                <a href="https://twitch.tv/Zajef77" class="underline text-blue-300">
+                    Zajef77
+                </a>
+            </p>
+        </div>
+    </footer>
 </template>
