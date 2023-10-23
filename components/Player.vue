95e662bff6233eb78383a97c3f3569f99208f645
probablyjassin <aouanijassin@gmail.com>
2023-10-23 09:20:34 +0200

    fix iframe secure

--- components/Player.vue
+++ components/Player.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<iframe width="1" height="1" src="http://www.youtube.com/embed/pHYHyZS_Xzo?enablejsapi=1&loop=1" frameborder="0" allowfullscreen class=""></iframe>
+		<iframe width="1" height="1" src="https://www.youtube.com/embed/pHYHyZS_Xzo?enablejsapi=1&loop=1" frameborder="0" allowfullscreen class=""></iframe>
 	</div>
 </template>
 
