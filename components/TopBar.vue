0b3f0350b23a624a0ee6d3d51b70167af36cba77
probablyjassin <aouanijassin@gmail.com>
2023-10-23 10:26:44 +0200

    change music icon

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -1,46 +1,45 @@
 <template>
-    <div class="absolute top-0 right-0 flex left-0 text-center text-white bg-slate-700 w-full p-1 space-x-10">
-        <div v-for="page in pages" :key="page.url" class="ml-10">
-            <nuxt-link :to="page.url" class="hover:bg-slate-800 p-1 rounded-xl">
-                {{ page.title }}
-            </nuxt-link>
-        </div>
-        <fa-icon :icon="['fas', 'wave-square']" class="icon wave-icon" v-if="isPlaying" @click="pauseVideo" />
-		<fa-icon :icon="['fas', 'music']" class="icon" v-else @click="playVideo" />
-        <Player v-if="showFrame"/>
-    </div>
+	<div class="absolute top-0 right-0 flex left-0 text-center text-white bg-slate-700 w-full p-1 space-x-10">
+		<div v-for="page in pages" :key="page.url" class="ml-10">
+			<nuxt-link :to="page.url" class="hover:bg-slate-800 p-1 rounded-xl">
+				{{ page.title }}
+			</nuxt-link>
+		</div>
+		<fa-icon :icon="['fas', 'wave-square']" class="icon wave-icon" v-if="isPlaying" @click="pauseVideo" />
+		<fa-icon :icon="['fab', 'itunes-note']" class="icon" v-else @click="playVideo" />
+		<Player v-if="showFrame" />
+	</div>
 </template>
 
 <script setup>
-const pages = [
-    {
-        title: 'Daily',
-        url: '/'
-    },
-    {
-        title: 'Endless',
-        url: '/endless'
-    }
-]
+	const pages = [
+		{
+			title: "Daily",
+			url: "/",
+		},
+		{
+			title: "Endless",
+			url: "/endless",
+		},
+	];
 
-const isPlaying = useState('music')
-let iframe;
-const showFrame = ref(false) 
+	const isPlaying = useState("music");
+	let iframe;
+	const showFrame = ref(false);
 
-const playVideo = () => {
-    if (!showFrame.value) {
-        showFrame.value = true
-        setTimeout(() => {
-            iframe = document.querySelector("iframe").contentWindow;
-        }, 250);
-        setTimeout(() => {
-            iframe.postMessage('{"event":"command","func":"playVideo","args":""}', "*");
-        }, 1000);
-    }
-    else {
-        iframe.postMessage('{"event":"command","func":"playVideo","args":""}', "*");
-    }
-    isPlaying.value = true;
+	const playVideo = () => {
+		if (!showFrame.value) {
+			showFrame.value = true;
+			setTimeout(() => {
+				iframe = document.querySelector("iframe").contentWindow;
+			}, 250);
+			setTimeout(() => {
+				iframe.postMessage('{"event":"command","func":"playVideo","args":""}', "*");
+			}, 1000);
+		} else {
+			iframe.postMessage('{"event":"command","func":"playVideo","args":""}', "*");
+		}
+		isPlaying.value = true;
 	};
 
 	const pauseVideo = () => {
@@ -52,16 +51,13 @@ const playVideo = () => {
 <style scoped>
 	@keyframes wave {
 		0% {
-			transform: translateY(-5px);
+			transform: translateY(2px);
 		}
-		33% {
-			transform: translateY(0px);
+		50% {
+			transform: translateY(-2px);
 		}
-        66% {
-			transform: translateY(5px);
-		}
-        100% {
-			transform: translateY(-5px);
+		100% {
+			transform: translateY(2px);
 		}
 	}
 
@@ -71,6 +67,6 @@ const playVideo = () => {
 	.icon {
 		width: 20px;
 		height: 20px;
-        cursor: pointer;
+		cursor: pointer;
 	}
 </style>
--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,8 +1,9 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faMusic, faWaveSquare } from '@fortawesome/free-solid-svg-icons'
+import { faItunesNote } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faMusic, faWaveSquare,)
+library.add(faItunesNote, faWaveSquare,)
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false
