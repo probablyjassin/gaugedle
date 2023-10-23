f371466f8e0603c635fe549a5ea06ccee77331f2
probablyjassin <aouanijassin@gmail.com>
2023-10-23 09:27:23 +0200

    still

--- components/Player.vue
+++ components/Player.vue
@@ -1,16 +1,7 @@
 <template>
 	<div>
-		<iframe width="1" height="1" src="https://www.youtube.com/embed/pHYHyZS_Xzo?enablejsapi=1&loop=1" frameborder="0" allowfullscreen class=""></iframe>
+		<iframe width="1" height="1" src="https://www.youtube.com/embed/pHYHyZS_Xzo?enablejsapi=1" frameborder="0" allowfullscreen class=""></iframe>
 	</div>
 </template>
-
-<script setup>	
-	const isPlaying = useState('music');
-	let iframe;
-	
-	onMounted(() => {
-		iframe = document.querySelector("iframe").contentWindow;
-	});
-</script>
 
 
