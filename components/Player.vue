151e15dbff1cbc080a66c2c696347965df788e86
probablyjassin <aouanijassin@gmail.com>
2023-10-23 09:39:38 +0200

    ?????

--- components/Player.vue
+++ components/Player.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<iframe width="100" height="100" src="https://www.youtube.com/embed/pHYHyZS_Xzo?enablejsapi=1" frameborder="0" allowfullscreen class=""></iframe>
+		<iframe width="1" height="1" src="https://www.youtube.com/embed/pHYHyZS_Xzo?enablejsapi=1" frameborder="0" allowfullscreen class=""></iframe>
 	</div>
 </template>
 
