cc76b7fc745e36849b66018d408effac6c4cb52c
probablyjassin <aouanijassin@gmail.com>
2023-10-23 19:34:36 +0200

    loop music

--- components/Player.vue
+++ components/Player.vue
@@ -1,5 +1,5 @@
 <template>
 	<div>
-		<iframe width="1" height="1" src="https://www.youtube.com/embed/pHYHyZS_Xzo?enablejsapi=1" frameborder="0" allowfullscreen class=""></iframe>
+		<iframe width="1" height="1" src="https://www.youtube.com/embed/pHYHyZS_Xzo?enablejsapi=1&version=3&loop=1&playlist=pHYHyZS_Xzo" frameborder="0" allowfullscreen class=""></iframe>
 	</div>
 </template>
