aff28adfd6a96d6ed2b5566fa870f0df1c92d343
probablyjassin <aouanijassin@gmail.com>
2023-11-10 11:49:58 +0100

    small fix

--- composables/useUtils.js
+++ composables/useUtils.js
@@ -3,6 +3,7 @@ const { abilities } = useAbilities()
 export function useUtils() {
 
     function pretty(input) {
+        if (!input) return ''
 		return input.replace(/([A-Z](?=[a-z\d])|\d+)/g, " $1").trim();
 	}
 
