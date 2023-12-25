729cf320cda0b779faa9f4d1371f9c4cbc6d1f4e
probablyjassin <aouanijassin@gmail.com>
2023-12-25 13:13:29 +0100

    +1 small fix

--- components/EleFilter.vue
+++ components/EleFilter.vue
@@ -1,5 +1,5 @@
 <template>
-	<button class="absolute right-28 top-[40%] transform -translate-y-1/2 px-3 invisible md:visible" @click="toggle" title="Filter by Element">
+	<button class="absolute right-28 top-[42%] transform -translate-y-1/2 px-3 invisible md:visible" @click="toggle" title="Filter by Element">
 		<faIcon icon="chevron-up" class="w-5 h-5 transition-all duration-200" :class="{ 'transform rotate-180': activated }"></faIcon>
 	</button>
 	<span class="absolute bg-white w-72 h-8 right-28 -translate-y-9 flex justify-between px-2 rounded-lg" :class="{'hidden': !activated}">
